cc04382e36455ecb3ec060ddbce0a41317cf35ad
OrigamingWasTaken <contact@origaming.ch>
2025-04-06 12:48:28 +0200

    Comment out user command and add hyperlinks to tags

--- src/commands/user.cmd.ts
+++ src/commands/user.cmd.ts
@@ -6,16 +6,16 @@ import {
 	SlashCommandBuilder,
 } from 'discord.js';
 
-RegisterSlashCommand({
-	data: new SlashCommandBuilder()
-		.setName('user_command')
-		.setDescription('A user app test command')
-		.setContexts(
-			InteractionContextType.Guild,
-			InteractionContextType.BotDM,
-			InteractionContextType.PrivateChannel
-		),
-	execute(interaction: ChatInputCommandInteraction<CacheType>) {
-		interaction.reply({ content: 'Hello world!', flags: ['Ephemeral'] });
-	},
-});
+// RegisterSlashCommand({
+// 	data: new SlashCommandBuilder()
+// 		.setName('user_command')
+// 		.setDescription('A user app test command')
+// 		.setContexts(
+// 			InteractionContextType.Guild,
+// 			InteractionContextType.BotDM,
+// 			InteractionContextType.PrivateChannel
+// 		),
+// 	execute(interaction: ChatInputCommandInteraction<CacheType>) {
+// 		interaction.reply({ content: 'Hello world!', flags: ['Ephemeral'] });
+// 	},
+// });
